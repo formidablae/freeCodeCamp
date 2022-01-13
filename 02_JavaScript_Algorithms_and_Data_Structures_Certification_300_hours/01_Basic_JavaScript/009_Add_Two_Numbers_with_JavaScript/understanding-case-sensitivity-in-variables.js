9713a6f4b426dfff3cfd1c35efd0238bdf42658d
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-13 03:47:44 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 009 Understanding Case Sensitivity in Variables

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/009_Add_Two_Numbers_with_JavaScript/understanding-case-sensitivity-in-variables.js
@@ -0,0 +1,9 @@
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
