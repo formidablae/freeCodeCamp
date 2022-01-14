86ba5163c44010a5dae4e67c7c0326aa5ea7adae
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-14 08:50:50 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 039 Word Blank

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/039_Word_Blanks/word-blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "A " + myAdjective + " " + myNoun + " " + myVerb + " really " + myAdverb + "."; // Change this line
+// Only change code above this line
