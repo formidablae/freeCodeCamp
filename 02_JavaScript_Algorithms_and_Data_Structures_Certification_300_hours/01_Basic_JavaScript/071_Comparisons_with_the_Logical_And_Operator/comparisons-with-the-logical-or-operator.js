9aa21421f1e8b8b68be0e790bd07fb456fce02a3
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:18:01 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 071 Comparisons with the Logical Or Operator

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/071_Comparisons_with_the_Logical_And_Operator/comparisons-with-the-logical-or-operator.js
@@ -0,0 +1,12 @@
+function testLogicalOr(val) {
+    // Only change code below this line
+
+    if (val < 10 || val > 20) {
+        return "Outside";
+    }
+
+    // Only change code above this line
+    return "Inside";
+}
+
+testLogicalOr(15);
