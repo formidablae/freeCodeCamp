d6097bbd675894471f78246f6c5c8a3d58bf8f6f
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:21:03 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 073 Introducing Else If Statements

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/073_Introducing_Else_If_Statements/introducing-else-if-statements.js
@@ -0,0 +1,11 @@
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
