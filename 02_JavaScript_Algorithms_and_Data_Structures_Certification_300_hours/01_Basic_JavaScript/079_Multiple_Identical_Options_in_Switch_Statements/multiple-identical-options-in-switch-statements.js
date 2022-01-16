4bd1c94975f8cd20c58ab12225630ed9db1bb8cd
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:34:31 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 079 Multiple Identical Options in Switch Statements

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/079_Multiple_Identical_Options_in_Switch_Statements/multiple-identical-options-in-switch-statements.js
@@ -0,0 +1,25 @@
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+sequentialSizes(1);
