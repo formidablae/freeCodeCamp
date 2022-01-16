b418c703a74f3674a2a5e49678b28a50a97cd530
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:24:36 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 075 Chaining If Else Statements

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/075_Chaining_If_Else_Statements/chaining-if-else-statements.js
@@ -0,0 +1,17 @@
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else {
+        return "Huge"
+    }
+    // Only change code above this line
+}
+
+testSize(7);
