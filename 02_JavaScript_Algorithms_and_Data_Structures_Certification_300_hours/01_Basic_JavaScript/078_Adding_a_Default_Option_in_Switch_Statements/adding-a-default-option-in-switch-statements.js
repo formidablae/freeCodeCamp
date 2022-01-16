0d8a57f5e3fada3ac798ab29b7b16322abf4b2ac
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:32:09 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 078 Adding a Default Option in Switch Statements

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/078_Adding_a_Default_Option_in_Switch_Statements/adding-a-default-option-in-switch-statements.js
@@ -0,0 +1,22 @@
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+switchOfStuff(1);
