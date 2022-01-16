4220deaa5314444e6ecbc4def60804527ab9539e
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:29:02 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 076 Golf Code

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/076_Golf_Code/golf-code.js
@@ -0,0 +1,23 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes === 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes === par - 1) {
+        return names[2];
+    } else if (strokes === par) {
+        return names[3];
+    } else if (strokes === par + 1) {
+        return names[4];
+    } else if (strokes === par + 2) {
+        return names[5];
+    } else if (strokes >= par + 3) {
+        return names[6];
+    }
+    // Only change code above this line
+}
+
+golfScore(5, 4);
