936d1d018dbada774faafd08eb11ccf2e93a2a2a
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-16 21:19:33 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 072 Introducing Else Statements

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/072_Introducing_Else_Statements/introducing-else-statements.js
@@ -0,0 +1,15 @@
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+
+    if (val > 5) {
+        result = "Bigger than 5";
+    } else {
+        result = "5 or Smaller";
+    }
+
+    // Only change code above this line
+    return result;
+}
+
+testElse(4);
