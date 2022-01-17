a98516239a4866a051c2a834e06080aa5c31e937
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 12:45:14 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 088 Updating Object Properties

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/088_Updating_Object_Properties/updating-object-properties.js
@@ -0,0 +1,10 @@
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
