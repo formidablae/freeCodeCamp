1decc4a8527bdc9cd1bd028380c84c473901f59c
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 19:04:22 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 102 Nesting For Loops

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/102_Nesting_For_Loops/nesting-for-loops.js
@@ -0,0 +1,13 @@
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j]
+        }
+    }
+    // Only change code above this line
+    return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
