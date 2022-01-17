4372b075249de72c758205b3c43c7141af522915
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 12:56:11 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 095 Accessing Nested Arrays

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/095_Accessing_Nested_Arrays/accessing-nested-arrays.js
@@ -0,0 +1,20 @@
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
