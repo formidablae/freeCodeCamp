ef6bd9aefa03a4cf677e59dee871ec94ed604a0a
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 12:40:01 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 085 Accessing Object Properties with Dot Notation

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/085_Accessing_Object_Properties_with_Dot_Notation/accessing-object-properties-with-dot-notation.js
@@ -0,0 +1,10 @@
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
