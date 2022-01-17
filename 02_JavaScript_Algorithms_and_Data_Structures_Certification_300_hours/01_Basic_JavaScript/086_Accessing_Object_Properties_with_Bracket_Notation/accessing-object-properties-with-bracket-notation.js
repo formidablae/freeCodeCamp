94ef15b9182dcef50a68ebd7d72fe64ba4bdf142
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 12:42:32 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 086 Accessing Object Properties with Bracket Notation

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/086_Accessing_Object_Properties_with_Bracket_Notation/accessing-object-properties-with-bracket-notation.js
@@ -0,0 +1,10 @@
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
