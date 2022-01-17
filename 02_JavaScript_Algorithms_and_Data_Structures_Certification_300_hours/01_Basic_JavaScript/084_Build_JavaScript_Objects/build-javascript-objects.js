9dec26e0cf133991a7876c5e636792e1d1832a11
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 12:38:23 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 084 Build JavaScript Objects

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/084_Build_JavaScript_Objects/build-javascript-objects.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    name: "Coco",
+    legs: 4,
+    tails: 1,
+    friends: ["Valentino", "Naomi", "Mata"],
+    // Only change code above this line
+};
