deeedafc0e6283cacce90c8e5afdea2b214d7345
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-17 12:53:47 +0100

    Did 02 JavaScript Algorithms and Data Structures / 01 Basic JavaScript / 093 Manipulating Complex Objects

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/01_Basic_JavaScript/093_Manipulating_Complex_Objects/manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Guns N' Roses",
+        "title": "November Rain",
+        "release_year": 1991,
+        "formats": [
+            "CD",
+            "LP"
+        ],
+    }
+];
