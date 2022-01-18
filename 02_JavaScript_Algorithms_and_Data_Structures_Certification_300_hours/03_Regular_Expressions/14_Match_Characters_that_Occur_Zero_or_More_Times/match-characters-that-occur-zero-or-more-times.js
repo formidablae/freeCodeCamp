ba4780d3dd7fdf91d89862bb9e562bfb2aa4cd42
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-18 18:56:33 +0100

    Did 02 JavaScript Algorithms and Data Structures / 03 Regular Expressions / 14 Match Characters that Occur Zero or More Times

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/03_Regular_Expressions/14_Match_Characters_that_Occur_Zero_or_More_Times/match-characters-that-occur-zero-or-more-times.js
@@ -0,0 +1,6 @@
+const chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
