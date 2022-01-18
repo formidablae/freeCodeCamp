7873ab1085c21d820871fc10b95267feb440edf5
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-18 01:46:45 +0100

    Did 02 JavaScript Algorithms and Data Structures / 02 ES6 / 18 Write Concise Declarative Functions with ES6

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/02_ES6/18_Write_Concise_Declarative_Functions_with_ES6/write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
