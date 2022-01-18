1f2a408c52a448dd522a76ec8d0a7859a9df103c
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-18 02:04:55 +0100

    Did 02 JavaScript Algorithms and Data Structures / 02 ES6 / 28 Complete a Promise with resolve and reject

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/02_ES6/28_Complete_a_Promise_with_resolve_and_reject/complete-a-promise-with-resolve-and-reject.js
@@ -0,0 +1,12 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if (responseFromServer) {
+        // Change this line
+        resolve("We got the data");
+    } else {
+        // Change this line
+        reject("Data not received");
+    }
+});
