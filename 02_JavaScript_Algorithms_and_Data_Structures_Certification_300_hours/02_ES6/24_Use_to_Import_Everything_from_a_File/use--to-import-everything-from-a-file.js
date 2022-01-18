9de89b259ef635b289528084df7d4cb2bb193045
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-18 01:56:33 +0100

    Did 02 JavaScript Algorithms and Data Structures / 02 ES6 / 24 Use * to Import Everything from a File

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/02_ES6/24_Use_to_Import_Everything_from_a_File/use--to-import-everything-from-a-file.js
@@ -0,0 +1,5 @@
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
