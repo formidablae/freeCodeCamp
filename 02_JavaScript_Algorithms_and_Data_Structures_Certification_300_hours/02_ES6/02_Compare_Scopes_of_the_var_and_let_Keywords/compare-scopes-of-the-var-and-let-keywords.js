58c061d1ed6da40a1c7cf3cd5747d22d6fa88bbb
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-18 01:14:12 +0100

    Did 02 JavaScript Algorithms and Data Structures / 02 ES6 / 02 Compare Scopes of the var and let Keywords

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/02_ES6/02_Compare_Scopes_of_the_var_and_let_Keywords/compare-scopes-of-the-var-and-let-keywords.js
@@ -0,0 +1,9 @@
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
