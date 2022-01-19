3053bf37647e6e01d2ee75822259029cddf85e71
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 00:55:48 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 10 Combine Arrays with the Spread Operator

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/10_Combine_Arrays_with_the_Spread_Operator/combine-arrays-with-the-spread-operator.js
@@ -0,0 +1,7 @@
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    return sentence;
+}
+
+console.log(spreadOut());
