fefa3821baa08d32b2140a5c430b36c3f7359185
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 00:59:49 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 12 Iterate Through All an Array's Items Using For Loops

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/12_Iterate_Through_All_an_Array_s_Items_Using_For_Loops/iterate-through-all-an-arrays-items-using-for-loops.js
@@ -0,0 +1,13 @@
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf(elem) < 0) {
+            newArr.push(arr[i]);
+        }
+    }
+    // Only change code above this line
+    return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
