7d9d8108f3e8a0c158945951b01b81810c7e0178
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 00:57:21 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 11 Check For The Presence of an Element With indexOf()

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/11_Check_For_The_Presence_of_an_Element_With_indexOf()/check-for-the-presence-of-an-element-with-indexof.js
@@ -0,0 +1,7 @@
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    return arr.indexOf(elem) >= 0;
+    // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
