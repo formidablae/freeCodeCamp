df5f3a348145eb40f1a4dd167321e01a4497b94b
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-19 01:50:32 +0100

    Did 02 JavaScript Algorithms and Data Structures / 03 Regular Expressions / 31 Check For Mixed Grouping of Characters

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/03_Regular_Expressions/31_Check_For_Mixed_Grouping_of_Characters/check-for-mixed-grouping-of-characters.js
@@ -0,0 +1,4 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
