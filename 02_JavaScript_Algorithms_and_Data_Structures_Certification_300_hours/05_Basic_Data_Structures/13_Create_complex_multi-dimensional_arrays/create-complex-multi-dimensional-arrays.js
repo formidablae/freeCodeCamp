b97a9ca65eb483759a8e6473d216a34dcac677e8
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 01:02:03 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 13 Create complex multi-dimensional arrays

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/13_Create_complex_multi-dimensional_arrays/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,17 @@
+let myNestedArray = [
+    // Only change code below this line
+    'unshift', false, 1, 2, 3, 'complex', 'nested',
+    [
+        'loop', 'shift', 6, 7, 1000, 'method',
+        [
+            'concat', false, true, 'spread', 'deep',
+            [
+                'mutate', 1327.98, 'splice', 'slice', 'deeper',
+                [
+                    'iterate', 1.3849, 7, '8.4876', 'arbitrary', 'deepest'
+                ]  
+            ]
+        ]
+    ]
+    // Only change code above this line
+];
