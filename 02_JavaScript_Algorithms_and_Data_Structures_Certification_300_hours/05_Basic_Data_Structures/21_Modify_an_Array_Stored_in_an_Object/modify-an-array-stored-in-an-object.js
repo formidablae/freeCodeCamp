6316d53a6f7a6293d3d3e2dbfe198fa6a8bccc6f
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 01:20:34 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 21 Modify an Array Stored in an Object

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/21_Modify_an_Array_Stored_in_an_Object/modify-an-array-stored-in-an-object.js
@@ -0,0 +1,28 @@
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+
+function addFriend(userObj, friend) {
+    // Only change code below this line
+    userObj.data.friends.push(friend);
+    return userObj.data.friends;
+    // Only change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));
