15093a48499e88c0bb751fd8f2892625213d6a7d
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 01:04:26 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 14 Add Key-Value Pairs to JavaScript Objects

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/14_Add_Key-Value_Pairs_to_JavaScript_Objects/add-key-value-pairs-to-javascript-objects.js
@@ -0,0 +1,13 @@
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+
+// Only change code below this line
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+// Only change code above this line
+
+console.log(foods);
