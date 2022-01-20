aa90ebe73a0e49594f773e9cc5f62b0313717a29
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-20 01:09:24 +0100

    Did 02 JavaScript Algorithms and Data Structures 300 hours / 05 Basic Data Structures / 16 Access Property Names with Bracket Notation

---
+++ 02_JavaScript_Algorithms_and_Data_Structures_Certification_300_hours/05_Basic_Data_Structures/16_Access_Property_Names_with_Bracket_Notation/access-property-names-with-bracket-notation.js
@@ -0,0 +1,16 @@
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+    // Only change code below this line
+    return foods[scannedItem];
+    // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
