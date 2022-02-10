f1d6e2ba802bc23af37a846b6c640cc5315ec537
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-10 04:46:09 +0100

    Did 07 Scientific Computing with Python / 1 Python for Everybody / 35 Networking: Using urllib in Python

---
+++ 07_Scientific_Computing_with_Python_Certification_300_hours/1_Python_for_Everybody/35_Networking_Using_urllib_in_Python/main.py
@@ -0,0 +1,7 @@
+import urllib.error
+import urllib.parse
+import urllib.request
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
