65e87328ff39f3bdaa811bed30f13c5b33879bd5
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-10 04:54:50 +0100

    Did 07 Scientific Computing with Python / 1 Python for Everybody / 36 Networking: Web Scraping with Python

---
+++ 07_Scientific_Computing_with_Python_Certification_300_hours/1_Python_for_Everybody/36_Networking_Web_Scraping_with_Python/main.py
@@ -0,0 +1,15 @@
+import urllib.error
+import urllib.parse
+import urllib.request
+from cgitb import html
+
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
