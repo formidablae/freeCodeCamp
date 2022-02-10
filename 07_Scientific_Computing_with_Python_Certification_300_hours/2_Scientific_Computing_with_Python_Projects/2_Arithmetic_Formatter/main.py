6e54be48369c98876a1b60e2638cb6ce8c4229e2
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-10 22:22:52 +0100

    Did 07 Scientific Computing with Python / 2 Scientific Computing with Python Projects / 2 Arithmetic Formatter Project

---
+++ 07_Scientific_Computing_with_Python_Certification_300_hours/2_Scientific_Computing_with_Python_Projects/2_Arithmetic_Formatter/main.py
@@ -0,0 +1,9 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from arithmetic_arranger import arithmetic_arranger
+from pytest import main
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+# Run unit tests automatically
+main()
